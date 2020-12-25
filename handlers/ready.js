49acc573334231a24a30b0835e95198bfe04c1b1
AsafSivan1 <asafisivan@gmail.com>
2020-12-25 15:36:53 +0200

    Update ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,6 +1,6 @@
 const {client} = require("../index");
 
 client.on("ready",function() {
-    client.user.setActivity("ON TESTS")
+    client.user.setActivity("made by @AsafSivan1 https://github.com/AsafSivan1/Discord-js-bot-base/edit/main/handlers/ready.js")
     console.log(`${client.user.username} Is Ready`)
 })
